ebea23a33a943de0cc61ae3bb7647057d688d313
Sambhav Kaushik <92575005+masterboy376@users.noreply.github.com>
2022-09-28 14:55:12 +0000

    number problems

--- basic-ptoblems/pattern-questions.cpp
+++ basic-ptoblems/pattern-questions.cpp
@@ -34,6 +34,26 @@ void printBinary(int n)
     }
     cout << "-----------------------------------------------" << endl;
 }
+void printRay(int n)
+{
+    cout << "-----------------------------------------------" << endl;
+    for (int i = 1; i <= 3; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            if (((i + j) % 4 == 0)||((i==2)&&(j%4==0)))
+            {
+                cout << "*";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+    cout << "-----------------------------------------------" << endl;
+}
 void printRohmbus(int n)
 {
     cout << "-----------------------------------------------" << endl;
@@ -125,6 +145,7 @@ int main()
     cin >> n;
     printInvertedNumber(n);
     printBinary(n);
+    printRay(n);
     printRohmbus(n);
     printNumberPiramid(n);
     printPalindromicPiramid(n);
