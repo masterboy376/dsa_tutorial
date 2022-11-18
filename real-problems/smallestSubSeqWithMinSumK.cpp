1a753d0b6a7c7949f38ad44d146a91a7ec11143f
Sambhav Kaushik <92575005+masterboy376@users.noreply.github.com>
2022-11-18 11:53:02 +0530

    Create smallestSubSeqWithMinSumK.cpp

---
+++ real-problems/smallestSubSeqWithMinSumK.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  int n,k; cin>>n>>k;
+
+  vector<int> a(n);
+  for(int i=0; i<n; i++){
+    cin>>a[i];
+  }
+
+  priority_queue<int, vector<int>> pq;
+  for(int i=0; i<n; i++){
+    pq.push(a[i]);
+  }
+
+  int sum = 0;
+  int count = 0;
+  while (!pq.empty()){
+    sum+=pq.top();
+    pq.pop();
+
+    count++;
+    
+    if(sum>=k){
+      break;
+    }
+  }
+
+  if(sum<k){
+    cout<<"-1"<<endl;
+  }
+  else{
+    cout<<count<<endl;
+  }
+  
+  return 0;
+}
