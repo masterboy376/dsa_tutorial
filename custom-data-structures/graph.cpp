6f3d7f76cd387f60b963ac22293063c4ef4691d0
Sambhav Kaushik <92575005+masterboy376@users.noreply.github.com>
2022-12-01 13:42:47 +0530

    Create graph.cpp

---
+++ custom-data-structures/graph.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+graphs:
+-nodes: yhese are the states or vertex.
+-edges: links between states in a graph.
+  -undirected: two way edges.
+  -directed: one way edges.
+
+-representation:
+  -adjacency matrix: 2d array where a[i][j]=1 if there is    an edge from i to j else a[i][j]=0.
+  -adjacency list: array of list, where each a[i] is a       list of nodes x<i> that are reachable from i.
+
+-searching:
+  -depth first search:
+  -breath first search:
+
+-pre-order: first time when call comes on the node.
+-in-order: when the call works inside the sebtree (in DFS 
+ tree) of that node.
+-post-order: after working on the subtree, call comes 
+ back to the node. comes on the same node.
+*/
+
+int main() {
+
+  return 0;
+}
