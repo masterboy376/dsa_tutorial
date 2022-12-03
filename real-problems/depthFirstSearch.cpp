d73adaab2e216723245ff5a9bb4291eded5cb160
Sambhav Kaushik <92575005+masterboy376@users.noreply.github.com>
2022-12-03 18:54:02 +0530

    Create depthFirstSearch.cpp

---
+++ real-problems/depthFirstSearch.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int N = 1e5+2;
+bool vis[N];
+vector<int> adj[N];
+
+void dfs(int node){
+  //preorder
+  vis[node] = 1;
+  // cout<<node<<" ";
+
+  //inorder
+  vector<int> :: iterator it;
+  for(it = adj[node].begin(); it!=adj[node].end(); it++){
+    if(vis[*it]!=1){
+      dfs(*it);
+    }
+  }
+
+  //preorder
+  cout<<node<<" ";
+  
+}
+
+int main() {
+  // breadth first search
+  for(int i = 0;i<N; i++){
+    vis[i]=0;
+  }
+  int n,m;
+  cin>>n>>m;
+
+  int x,y;
+  for(int i=0; i<m; i++){
+    cin>>x>>y;
+    adj[x].push_back(y);
+    adj[y].push_back(x);
+  }
+
+  dfs(1);
+  
+  
+  return 0;
+}
