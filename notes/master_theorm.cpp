6b1a27204766469acaa73dda5d1c820f7848c3ed
Sambhav Kaushik <92575005+masterboy376@users.noreply.github.com>
2023-10-07 17:33:30 +0530

    Update and rename master_theorm.cpp to master_theorm.txt

--- notes/master_theorm.cpp
+++
@@ -1,5 +0,0 @@
-/*
-Recurrence relation: is an equation that recursively defines a sequence. for eg: F(n) = F(n-1)+F(n-2)
-Master Theorm: gives time complexity for the recurrence relation T(n) = aT(n/b) + f(n)
-- There are 3 cases 
-*/
